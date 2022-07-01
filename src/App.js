a82992bb5fb3e082c98770d8993e6336e458dd5a
Danilo Martins <dansm86@outlook.com>
2022-07-01 20:08:37 -0300

    Req2-Implemented the login action and redirect...
    Redirection to search page.
    Showing the loading message while redirecting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { createUser } from './services/userAPI';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -9,12 +10,64 @@ import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profile: {
+        name: '',
+      },
+      isLoading: false,
+      isLoggedIn: false,
+    };
+  }
+
+  handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      profile: {
+        [name]: value,
+      },
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  handleLogin = () => {
+    const { profile } = this.state;
+    this.setState({
+      isLoading: true,
+    }, () => {
+      createUser(profile)
+        .then(() => {
+          this.setState({
+            isLoggedIn: true,
+            isLoading: false,
+          });
+        });
+    });
+  }
+
   render() {
+    const { profile, isLoading, isLoggedIn } = this.state;
     return (
       <div>
         <h1>Trybe Tunes</h1>
         <Switch>
-          <Route exact path="/" component={ Login } />
+          <Route
+            exact
+            path="/"
+            render={ (routeProps) => (<Login
+              { ...routeProps }
+              onInputChange={ this.handleInputChange }
+              onSubmit={ this.handleSubmit }
+              onClickBtn={ this.handleLogin }
+              profile={ profile }
+              isLoading={ isLoading }
+              isLoggedIn={ isLoggedIn }
+            />) }
+          />
           <Route path="/search" component={ Search } />
           <Route path="/album/:id" component={ Album } />
           <Route path="/favorites" component={ Favorites } />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import InputLogin from '../components/InputLogin';
+import Loading from '../components/Loading';
 
 class Login extends Component {
   render() {
+    const { isLoading, isLoggedIn } = this.props;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        {
+          !isLoading ? <InputLogin { ...this.props } /> : <Loading />
+        }
+        {
+          isLoggedIn && <Redirect push to="/search" />
+        }
+      </div>
     );
   }
 }
