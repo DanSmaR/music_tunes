a39b65627b0da79428662b36e78adb159a77fd60
Danilo Martins <dansm86@outlook.com>
2022-07-02 00:38:10 -0300

    Added sematinc tags to Header like nav tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,12 +26,25 @@ export default class Header extends Component {
     const { isLoading, profile: { name } } = this.state;
     return (
       <header data-testid="header-component">
-        <NavLink data-testid="link-to-search" to="/search">Pesquisa</NavLink>
-        <NavLink data-testid="link-to-favorites" to="/favorites">Favoritos</NavLink>
-        <NavLink data-testid="link-to-profile" to="/profile">Perfil</NavLink>
-        {
-          isLoading ? <Loading /> : <span data-testid="header-user-name">{ name }</span>
-        }
+        <div>
+          <h1>Trybe Tunes</h1>
+          {
+            isLoading ? <Loading /> : <span data-testid="header-user-name">{ name }</span>
+          }
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink data-testid="link-to-search" to="/search">Pesquisa</NavLink>
+            </li>
+            <li>
+              <NavLink data-testid="link-to-favorites" to="/favorites">Favoritos</NavLink>
+            </li>
+            <li>
+              <NavLink data-testid="link-to-profile" to="/profile">Perfil</NavLink>
+            </li>
+          </ul>
+        </nav>
       </header>
     );
   }
